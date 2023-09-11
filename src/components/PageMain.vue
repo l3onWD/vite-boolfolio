a8e1dabed4033a3c502f544691f26e21af83ec6b
l3on <l3onwebdev@gmail.com>
2023-09-11 17:47:18 +0200

    changed card layout

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -17,15 +17,21 @@ export default {
 
 <template>
     <main class="container my-4">
-        <h1 class="mb-4">Lista Progetti</h1>
 
-        <div v-if="store.projects.data.length" class="row row-cols-3 g-3">
-            <div v-for="project in store.projects.data" :key="project.id" class="col">
-                <ProjectCard :project="project" />
+        <section>
+
+            <h1 class="mb-4">Lista Progetti</h1>
+
+            <div v-if="store.projects.data.length" class="row-cols-1">
+                <div v-for="project in store.projects.data" :key="project.id" class="col mb-3">
+                    <ProjectCard :project="project" />
+                </div>
             </div>
-        </div>
 
-        <h3 v-else>Non ci sono progetti</h3>
+            <h3 v-else>Non ci sono progetti</h3>
+
+        </section>
+
     </main>
 </template>
 
--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -6,27 +6,45 @@ export default {
 
 
 <template>
-    <div class="project-card rounded shadow h-100 p-3">
+    <div class="project-card">
 
-        <!-- Image -->
-        <div v-if="project.screenshot" class="mb-3">
-            <img :src="project.screenshot" :alt="project.name" class="img-fluid">
-        </div>
 
-        <!-- Body -->
-        <div>
-            <h4>{{ project.name }}</h4>
+        <!-- Card Top -->
+        <div class="clearfix mb-3">
+            <!-- Screenshot -->
+            <img v-if="project.screenshot" :src="project.screenshot" :alt="project.name" class="img-fluid float-start me-3">
+
+            <!-- Name -->
+            <h4 class="mb-3">{{ project.name }}</h4>
+
+            <!-- Type -->
             <p v-if="project.type">
-                <span class="badge" :style="`background-color: ${project.type.color}`">{{ project.type.label }}</span>
+                <span class="badge" :style="`background-color: ${project.type.color}`">{{ project.type.label
+                }}</span>
             </p>
         </div>
 
-        <!-- Footer -->
+        <!-- Card Footer -->
         <div>
+            <!-- Created At -->
             <p><strong>Creato il: </strong>{{ project.created_at }}</p>
         </div>
+
     </div>
 </template>
 
 
-<style></style>
+<style scoped lang="scss">
+.project-card {
+    min-height: 200px;
+    padding: 1rem;
+
+    border-radius: .5rem;
+
+    box-shadow: 0 0 8px 4px rgba($color: #000, $alpha: 0.08);
+
+    img {
+        width: 150px;
+    }
+}
+</style>
