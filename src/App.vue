4a58f50bfae546f14003341cd439deceaeba4d5e
l3on <l3onwebdev@gmail.com>
2023-09-11 16:49:04 +0200

    added projects fetching

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <script>
+import axios from 'axios';
+
 export default {
+    data: () => ({
+        projects: {
+            data: [],
+            links: []
+        }
+    }),
+    methods: {
+        fetchProjects(endpoint = 'http://127.0.0.1:8000/api/projects') {
+            axios.get(endpoint)
+                .then(res => {
+                    const { data, links } = res.data;
+                    this.projects = { data, links };
 
-
+                    console.log(this.projects.data);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
 }
 </script>
 
