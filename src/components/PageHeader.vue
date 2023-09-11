0594163c87cd68079f6e645d00f96b1879ebe08b
l3on <l3onwebdev@gmail.com>
2023-09-11 17:15:14 +0200

    added basic page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <header>
+        <!-- Navbar -->
+        <nav class="navbar bg-dark" data-bs-theme="dark">
+            <div class="container">
+
+                <!-- Brand -->
+                <a class="navbar-brand" href="#">Boolfolio</a>
+
+                <!-- Menu -->
+                <ul class="navbar-nav">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
