bc990d68b321d5947332163560b41c6f088d3899
l3on <l3onwebdev@gmail.com>
2023-09-11 17:31:34 +0200

    save projects to store

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,19 @@ import PageMain from './components/PageMain.vue';
 
 /*** DATA ***/
 import axios from 'axios';
+import { store } from './data/store';
 
 
 export default {
     components: { PageHeader, PageMain },
-    data: () => ({
-        projects: {
-            data: [],
-            links: []
-        }
-    }),
     methods: {
         fetchProjects(endpoint = 'http://127.0.0.1:8000/api/projects') {
             axios.get(endpoint)
                 .then(res => {
                     const { data, links } = res.data;
-                    this.projects = { data, links };
+                    store.projects = { data, links };
 
-                    console.log(this.projects.data);
+                    console.log(store.projects.data);
                 })
                 .catch(err => {
                     console.error(err);
