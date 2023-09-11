880bb0abb4910c4dca5d4eca466a09f6eb047dad
l3on <l3onwebdev@gmail.com>
2023-09-11 16:08:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <main class="container">
+        <h1>Hello World</h1>
+    </main>
+</template>
+
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
