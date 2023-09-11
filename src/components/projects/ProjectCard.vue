a47e41557206c731c41d0d7f0d5d0bc4800166c4
l3on <l3onwebdev@gmail.com>
2023-09-11 17:04:09 +0200

    added project card component

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: { project: Object }
+}
+</script>
+
+
+<template>
+    <div class="project-card rounded shadow h-100 p-3">
+
+        <!-- Image -->
+        <div v-if="project.screenshot" class="mb-3">
+            <img :src="project.screenshot" :alt="project.name" class="img-fluid">
+        </div>
+
+        <!-- Body -->
+        <div>
+            <h4>{{ project.name }}</h4>
+            <p v-if="project.type">
+                <span class="badge" :style="`background-color: ${project.type.color}`">{{ project.type.label }}</span>
+            </p>
+        </div>
+
+        <!-- Footer -->
+        <div>
+            <p><strong>Creato il: </strong>{{ project.created_at }}</p>
+        </div>
+    </div>
+</template>
+
+
+<style></style>
