dfee2c06cbde20b86e9ee250a46bdccb7a544156
l3on <l3onwebdev@gmail.com>
2023-09-11 18:20:43 +0200

    added basic pagination layout

---
+++ src/components/basic/BasicPagination.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: { links: Array },
+    emits: ['change-page']
+}
+</script>
+
+
+<template>
+    <nav>
+        <ul class="pagination">
+            <li v-for="link in links" :key="link.label" class="page-item"
+                :class="[{ active: link.active }, { disabled: !link.url }]">
+                <button :disabled="!link.url" @click="$emit('change-page', link.url)" class="page-link"
+                    v-html="link.label"></button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
