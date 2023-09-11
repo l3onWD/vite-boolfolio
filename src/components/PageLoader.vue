170b6bdc7c4e5b67eb1e0b8346acc78a0effd8ac
l3on <l3onwebdev@gmail.com>
2023-09-11 18:35:49 +0200

    added page loader component

---
+++ src/components/PageLoader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    props: { isActive: Boolean }
+}
+</script>
+
+
+<template>
+    <div v-if="isActive" class="loader">
+        <div class="spinner-border text-white" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+.loader {
+    position: fixed;
+    inset: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba($color: #000, $alpha: 0.8);
+
+    z-index: 5;
+
+    .spinner-border {
+        width: 100px;
+        height: 100px;
+    }
+}
+</style>
