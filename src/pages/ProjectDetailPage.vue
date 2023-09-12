64d51d3b3f4fcc807fd4685c61afdaeb0b13b358
l3on <l3onwebdev@gmail.com>
2023-09-12 16:24:00 +0200

    added detail component base data and methods

--- src/pages/ProjectDetailPage.vue
+++ src/pages/ProjectDetailPage.vue
@@ -1,13 +1,63 @@
 <script>
-export default {
+/* -----------------------------------------
+* RESOURCES
+-------------------------------------------*/
+/*** COMPONENTS ***/
+import PageLoader from '../components/PageLoader.vue';
+import ProjectCard from '../components/projects/ProjectCard.vue';
 
+/*** DATA ***/
+import axios from 'axios';
+
+
+export default {
+    components: { PageLoader, ProjectCard },
+    data: () => ({ project: null, loaderIsActive: false }),
+    methods: {
+
+        /**
+         * Retrieve Project from the API
+         * @param {String} endpoint 
+         * @param {Object} params 
+         */
+        fetchProject(endpoint = 'http://127.0.0.1:8000/api/projects/') {
+
+            // Show Loader
+            this.loaderIsActive = true;
+
+            // Fetching
+            axios.get(endpoint + this.$route.params.id)
+                .then(res => {
+                    this.project = res.data;
+                })
+                .catch(err => {
+                    console.error(err);
+
+                })
+                .then(() => {
+                    // Hide Loader
+                    this.loaderIsActive = false;
+                });
+        },
+    },
+    created() {
+        this.fetchProject();
+    }
 
 }
 </script>
 
 
 <template>
-    <h1>dettaglio</h1>
+    <!-- Page Main -->
+    <main class="container my-4">
+
+        <div>{{ project }}</div>
+
+    </main>
+
+    <!-- Page Loader -->
+    <PageLoader :isActive="loaderIsActive" />
 </template>
 
 
