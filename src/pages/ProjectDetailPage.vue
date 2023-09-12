fae97fa5ba7a344fc0dbcb65a9fd72bc67a1bafa
l3on <l3onwebdev@gmail.com>
2023-09-12 16:15:00 +0200

    added detail route and empty component detail page

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <h1>dettaglio</h1>
+</template>
+
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router
 
 // Pages
 import HomePage from '../pages/HomePage.vue';
+import ProjectDetailPage from '../pages/ProjectDetailPage.vue';
 
 
 // Routes
@@ -9,8 +10,10 @@ const router = createRouter({
     history: createWebHistory(),
     linkExactActiveClass: 'active',
     routes: [
-        { path: '/', name: 'home', component: HomePage }
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/projects/:id', name: 'project-detail', component: ProjectDetailPage }
     ]
+
 });
 
 export { router };
