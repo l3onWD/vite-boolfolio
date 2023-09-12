6cbc0fddef0e1f0a7fc4d3d075967eaafbd77ab0
l3on <l3onwebdev@gmail.com>
2023-09-12 15:23:07 +0200

    added home page component

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,78 @@
+<script>
+/* -----------------------------------------
+* RESOURCES
+-------------------------------------------*/
+/*** COMPONENTS ***/
+import PageMain from './components/PageMain.vue';
+import PageLoader from './components/PageLoader.vue';
+
+/*** DATA ***/
+import axios from 'axios';
+import { store } from './data/store';
+
+
+export default {
+    components: { PageMain, PageLoader },
+    data: () => ({ loaderIsActive: false }),
+    methods: {
+
+        /**
+         * Retrieve Projects from the API
+         * @param {String} endpoint 
+         * @param {Object} params 
+         */
+        fetchProjects(endpoint = 'http://127.0.0.1:8000/api/projects', params = {}) {
+
+            // Show Loader
+            this.loaderIsActive = true;
+
+            // Fetching
+            axios.get(endpoint, { params })
+                .then(res => {
+                    const { data, links } = res.data;
+                    store.projects = { data, links };
+                })
+                .catch(err => {
+                    console.error(err);
+
+                    // Vreate alert message
+                    store.alert.type = 'danger';
+                    store.alert.title = 'Attenzione!';
+                    store.alert.message = 'Il server non risponde.';
+                })
+                .then(() => {
+                    // Hide Loader
+                    this.loaderIsActive = false;
+                });
+        },
+
+
+        /**
+         * Retrieve Projects with filters stored
+         */
+        filterProjects() {
+            const params = {};
+
+            if (store.filters.name) params.filter_name = store.filters.name;
+
+            this.fetchProjects('http://127.0.0.1:8000/api/projects', params);
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+
+}
+</script>
+
+
+<template>
+    <!-- Page Main -->
+    <PageMain @projects-page-changed="fetchProjects" @projects-filters-changed="filterProjects" />
+
+    <!-- Page Loader -->
+    <PageLoader :isActive="loaderIsActive" />
+</template>
+
+
+<style></style>
