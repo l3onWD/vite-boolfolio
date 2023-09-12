d52fdc84a27163c7fa46cb56b8635b9a6a6dcb07
l3on <l3onwebdev@gmail.com>
2023-09-12 15:40:40 +0200

    added router view to app component

--- src/App.vue
+++ src/App.vue
@@ -4,65 +4,10 @@
 -------------------------------------------*/
 /*** COMPONENTS ***/
 import PageHeader from './components/PageHeader.vue';
-import PageMain from './components/PageMain.vue';
-import PageLoader from './components/PageLoader.vue';
-
-
-/*** DATA ***/
-import axios from 'axios';
-import { store } from './data/store';
 
 
 export default {
-    components: { PageHeader, PageMain, PageLoader },
-    data: () => ({ loaderIsActive: false }),
-    methods: {
-
-        /**
-         * Retrieve Projects from the API
-         * @param {String} endpoint 
-         * @param {Object} params 
-         */
-        fetchProjects(endpoint = 'http://127.0.0.1:8000/api/projects', params = {}) {
-
-            // Show Loader
-            this.loaderIsActive = true;
-
-            // Fetching
-            axios.get(endpoint, { params })
-                .then(res => {
-                    const { data, links } = res.data;
-                    store.projects = { data, links };
-                })
-                .catch(err => {
-                    console.error(err);
-
-                    // Vreate alert message
-                    store.alert.type = 'danger';
-                    store.alert.title = 'Attenzione!';
-                    store.alert.message = 'Il server non risponde.';
-                })
-                .then(() => {
-                    // Hide Loader
-                    this.loaderIsActive = false;
-                });
-        },
-
-
-        /**
-         * Retrieve Projects with filters stored
-         */
-        filterProjects() {
-            const params = {};
-
-            if (store.filters.name) params.filter_name = store.filters.name;
-
-            this.fetchProjects('http://127.0.0.1:8000/api/projects', params);
-        }
-    },
-    created() {
-        this.fetchProjects();
-    }
+    components: { PageHeader },
 }
 </script>
 
@@ -71,11 +16,8 @@ export default {
     <!-- Page Header -->
     <PageHeader />
 
-    <!-- Page Main -->
-    <PageMain @projects-page-changed="fetchProjects" @projects-filters-changed="filterProjects" />
-
-    <!-- Page Loader -->
-    <PageLoader :isActive="loaderIsActive" />
+    <!-- Page Router -->
+    <RouterView />
 </template>
 
 
