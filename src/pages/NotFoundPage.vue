8ad8f36b43994add20397b4f7b91e2c35ce402bc
l3on <l3onwebdev@gmail.com>
2023-09-12 17:25:04 +0200

    added not found page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+
+}
+</script>
+
+
+<template>
+    <!-- Page Main -->
+    <main class="d-flex">
+        <div class="m-auto text-center">
+            <h1 class="mb-4">404 | Pagina non trovata</h1>
+            <button @click="$router.back()" class="btn btn-primary">Torna Indietro</button>
+        </div>
+    </main>
+</template>
+
+
+<style scoped lang="scss">
+main {
+    height: calc(100vh - 56px);
+}
+</style>
