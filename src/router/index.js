424ee9c70a848d0ccde27e57989cb13e266dac53
l3on <l3onwebdev@gmail.com>
2023-09-12 15:40:31 +0200

    fixed router hystory setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 // Pages
 import HomePage from '../pages/HomePage.vue';
@@ -6,6 +6,7 @@ import HomePage from '../pages/HomePage.vue';
 
 // Routes
 const router = createRouter({
+    history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: HomePage }
     ]
